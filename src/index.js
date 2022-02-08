a5ca662afd0ddd103827a28b82fa878bb86a9ae8
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2022-02-08 17:21:02 -0500

    added 8 event listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import './less/index.less'
 
 // Your code goes here!
+const funBus = document.querySelector('h1');
+const funBusImage = document.querySelector('div > header > img');
+
+funBus.addEventListener('mousedown', ()=>{
+    funBus.style.fontSize = 'smaller';
+})
+funBus.addEventListener('mouseup', ()=>{
+    funBus.style.fontSize = 'xx-large';
+})
+
+document.querySelector('body').addEventListener('keydown',()=>{
+    if(document.body.style.color === 'blue'){
+        document.body.style.color = 'black';
+    }
+    else{
+        document.body.style.color = 'blue';
+    }
+});
+
+//adding random colors to nav
+document.querySelectorAll('nav>a').forEach((item) => {
+    const rndCol = `rgb(${Math.random()*255+1}, ${Math.random()*255+1}, ${Math.random()*255+1})`;
+    item.addEventListener('mouseover',()=>{
+        item.style.color = rndCol;
+    })
+    item.addEventListener('mouseout',()=>{
+        item.style.color = 'black';
+    })
+});
+
+document.querySelectorAll('h2, h4').forEach((item) => {
+    item.addEventListener('dblclick', ()=>{
+        item.style.backgroundColor = 'red';
+    });
+});
+
+window.onresize=()=>{
+    funBusImage.style.width = '50%';
+};
+
+window.onscroll=()=>{
+    funBusImage.style.width = '100%';
+}
